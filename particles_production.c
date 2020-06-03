7d704a4c4e8e9da8873d7def472ad5da426fc31b
nicolaDeCristofaro <nico.de.cristofaro97@gmail.com>
2020-06-03 18:06:33 +0200

    handling error in conversion string to int

--- particles_production.c
+++ particles_production.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <errno.h>
+#include <limits.h>
 
 typedef struct{
     float mass;
@@ -8,25 +10,17 @@ typedef struct{
     float vx, vy, vz;
 } Particle;
 
-/*Inizializza con valori random lo stato delle particelle*/
-void randomizeParticles(Particle *particles, int n) {
-    for (int i = 0; i < n; i++) {
-        particles[i].mass = 2.0;  // valore arbitrario scelto per la massa delle particelle -> 2.0
-        particles[i].x = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;  //numero random compreso tra -1 and 1
-		particles[i].y = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
-		particles[i].z = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
-        particles[i].vx = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
-	    particles[i].vy = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
-		particles[i].vz = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
-  }
-}
+/*Definizione di funzioni*/
+int convertStringToInt(char *str);
+void randomizeParticles(Particle *particles, int n);
 
 int main(int argc, char* argv[]){
 
     int num_particles = 1000;  //Numero delle particelle di DEFAULT se nessun parametro è fornito sulla command-line
     if(argc > 1){
         // E' stato fornito il parametro da linea di comando che indica il numero di particelle
-        num_particles = atoi(argv[1]);
+        //Converti stringa a intero
+        num_particles = convertStringToInt(argv[1]);
     }
 
     Particle *particles = NULL;
@@ -39,6 +33,7 @@ int main(int argc, char* argv[]){
     if (file != NULL) {
         fwrite(particles , sizeof(Particle) * num_particles, 1, file);
         fclose(file);
+        printf("%d particelle sono state create con valori random e scritte su file\n", num_particles);
     }
 
     free(particles);
@@ -54,4 +49,40 @@ int main(int argc, char* argv[]){
         printf("[%d].vz = %f\t", i, particles[i].vz);
         printf("\n");
     }*/
+}
+
+/*Conversione da stringa a intero*/
+int convertStringToInt(char *str){
+    char *endptr;
+    long val;  
+    errno = 0;  //Per distinguere successo/fallimento dopo la chiamata
+
+    val = strtol(str, &endptr, 10);
+
+    /* Check possibili errori */
+    if ((errno == ERANGE && (val == LONG_MAX || val == LONG_MIN)) || (errno != 0 && val == 0)) {
+        perror("strtol");
+        exit(EXIT_FAILURE);
+    }
+
+    if (endptr == str) {
+        fprintf(stderr, "No digits were found\n");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Se siamo qui, strtol() ha convertito un numero correttamente */
+    return (int)val;
+}
+
+/*Inizializza con valori random lo stato delle particelle*/
+void randomizeParticles(Particle *particles, int n) {
+    for (int i = 0; i < n; i++) {
+        particles[i].mass = 2.0;  // valore arbitrario scelto per la massa delle particelle -> 2.0
+        particles[i].x = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;  //numero random compreso tra -1 and 1
+		particles[i].y = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
+		particles[i].z = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
+        particles[i].vx = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
+	    particles[i].vy = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
+		particles[i].vz = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
+  }
 }
